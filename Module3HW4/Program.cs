808b515f3e0cd59522b363c308ba01121d60f873
Dubrov Ruslan <dubrov.ruslan@gmail.com>
2021-12-16 03:10:31 +0200

    Done

---
+++ Module3HW4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Module3HW4
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("TASK 1");
+            var task1 = new Task1();
+            task1.Subscribe();
+            task1.Run();
+            Console.WriteLine(task1.SumResult);
+            Console.WriteLine("TASK 2");
+            var task2 = new Task2();
+            task2.Test();
+        }
+    }
+}
---
+++ Module3HW4/Task1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Module3HW4
+{
+    public class Task1
+    {
+        private List<double> _results = new List<double>();
+        public event Func<double, double, double> MyEvent;
+
+        public double SumResult
+        {
+            get
+            {
+                var sumResult = 0.0;
+                foreach (var item in _results)
+                {
+                    sumResult += item;
+                }
+
+                return sumResult;
+            }
+        }
+
+        public void Subscribe()
+        {
+            MyEvent += Sum;
+            MyEvent += Sum;
+        }
+
+        public void Run()
+        {
+            TryMethod(() => MyEvent(3, 5));
+        }
+
+        public static void TryMethod(Action action)
+        {
+            try
+            {
+                action();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private double Sum(double x, double y)
+        {
+            var result = x + y;
+            _results.Add(result);
+            return result;
+        }
+    }
+}
